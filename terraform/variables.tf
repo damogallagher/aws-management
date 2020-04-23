8e36c7ba0ee24f019ed3af4769e7d3e0115593d9
Damien Gallagher <damiengallagher@Damiens-MacBook-Pro-16.local>
2020-04-23 12:19:44 +0100

    Removed old dir

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,22 +1,38 @@
-variable "name_prefix" {
-  description = "The prefix to be used for AWS resources"
-  default     = "awsmanagement"
-}
-variable "aws_profile" {
-  description = "The AWS profile to use"
-  default     = "default"
-}
 variable "aws_region" {
   description = "The AWS region things are created in"
   default     = "us-east-1"
 }
 
-variable "ecr_image_name" {
-  description = "The image to use in ecr"
-  default     = "858398790708.dkr.ecr.us-east-1.amazonaws.com/damiengallagher"
-}
-variable "ecr_image_tag" {
-  description = "The tag of the image to use in ecr"
-  default     = "latest"
+variable "az_count" {
+  description = "Number of AZs to cover in a given region"
+  default     = "2"
 }
 
+variable "app_image" {
+  description = "Docker image to run in the ECS cluster"
+  default     = "858398790708.dkr.ecr.us-east-1.amazonaws.com/damiengallagher:latest"
+}
+
+variable "app_port" {
+  description = "Port exposed by the docker image to redirect traffic to"
+  default     = 8080
+}
+
+variable "app_count" {
+  description = "Number of docker containers to run"
+  default     = 2
+}
+
+variable "health_check_path" {
+  default = "/actuator/health"
+}
+
+variable "fargate_cpu" {
+  description = "Fargate instance CPU units to provision (1 vCPU = 1024 CPU units)"
+  default     = "1024"
+}
+
+variable "fargate_memory" {
+  description = "Fargate instance memory to provision (in MiB)"
+  default     = "2048"
+}
